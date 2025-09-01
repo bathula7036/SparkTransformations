4a8fb27cf9c4913f695880252362789f8bd40308
Vijendar <137201987+bathula7036@users.noreply.github.com>
2025-08-31 19:50:09 -0500

    Update Sales Analysis - Remove Rows with Null Invoice Numbers.py

--- Sales Analysis - Remove Rows with Null Invoice Numbers.py
+++ Sales Analysis - Remove Rows with Null Invoice Numbers.py
@@ -1,1 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import * 
+from pyspark.sql.window import Window
+from pyspark.sql.types import *
+import datetime
+import random
 
+spark = SparkSession.builder.appName("DataMgmtApplication") \
+        .master("local[*]") \
+        .getOrCreate()
+
+sales_data = [
+(1001, "A101", 25.99),
+(None, "B202", 49.99),
+(1003, None, 15.49),
+(1004, "C303", 75.00)
+]
+
+sales = spark.createDataFrame(sales_data, ["invoice_id", "product_id", "amount"])
+sales.show()
+
+clean_sales = sales.dropna(subset=["invoice_id"])
+clean_sales.show()
+
+clean_sales = sales.dropna(subset= ["product_id"])
+clean_sales.show()
